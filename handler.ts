b259850a2a1c9b2cc5269cfa81e7ee5fcf0dd6f1
Batuhan Ozyon <batuhanozyon@gmail.com>
2022-11-27 21:47:31 +0300

    handler

--- handler.ts
+++ handler.ts
@@ -1,14 +1,27 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 export const hello = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const pageTitle = await page.title();
+  await browser.close();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: pageTitle,
         input: event,
       },
       null,
